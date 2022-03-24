e153f629a26310707a76757bedc81ca9bd1622ca
Ivar Refsdal <refsdal.ivar@gmail.com>
2022-03-24 11:37:37 +0100

    Initial commit

---
+++ src/com/github/ivarref/healthy.clj
@@ -0,0 +1,46 @@
+(ns com.github.ivarref.healthy
+  (:import (java.time Duration)))
+
+(def config (atom {}))
+(def errors (atom {}))
+
+(def ^:dynamic *now-ms* (fn [] (System/currentTimeMillis)))
+
+(declare drop-old-errors-impl! add-error!-impl)
+
+(defn init! [{:keys [duration]
+              :as _cfg}]
+  (let [duration (if (string? duration)
+                   (Duration/parse duration)
+                   duration)]
+    (assert (instance? Duration duration) "Expected :duration to be of type java.time.Duration")
+    (let [millis (.toMillis ^Duration duration)]
+      (assert (pos-int? millis) "Duration must be positive/non-zero")
+      (assert (>= millis 60e3) "Duration must be at least 60 seconds")
+      (reset! config {:duration millis})
+      (reset! errors []))))
+
+(defn error-count []
+  (reduce + 0 (vals (drop-old-errors-impl! @config (*now-ms*) errors))))
+
+(defn add-error! []
+  (add-error!-impl @config (*now-ms*) errors))
+
+
+(defn- remove-old [duration now-ms errs]
+  (reduce-kv (fn [m k v]
+               (if (<= k (- now-ms duration))
+                 m
+                 (assoc m k v)))
+             {}
+             errs))
+
+(defn add-error!-impl [{:keys [duration]} now-ms err-atom]
+  (let [bucket (long (* 60e3 (long (/ now-ms 60e3))))]
+    (swap! err-atom
+           (fn [errs]
+             (->> (update errs bucket (fnil inc 0))
+                  (remove-old duration now-ms))))))
+
+(defn drop-old-errors-impl! [{:keys [duration]} now-ms err-atom]
+  (swap! err-atom (partial remove-old duration now-ms)))
---
+++ test/com/github/ivarref/healthy_test.clj
@@ -0,0 +1,24 @@
+(ns com.github.ivarref.healthy-test
+  (:require [clojure.test :refer [deftest is]]
+            [com.github.ivarref.healthy :as h])
+  (:import (java.time Duration)))
+
+(deftest basic
+  (h/init! {:duration "PT15M"})
+  (is (= 0 (h/error-count)))
+
+  (h/add-error!)
+  (is (= 1 (h/error-count)))
+
+  (h/add-error!)
+  (is (= 2 (h/error-count)))
+
+  (with-bindings {#'h/*now-ms*
+                  (fn [] (+ (System/currentTimeMillis)
+                            (.toNanos (Duration/parse "PT15M"))))}
+    (is (= 0 (h/error-count)))
+
+    (h/add-error!)
+    (h/add-error!)
+    (h/add-error!)
+    (is (= 3 (h/error-count)))))
